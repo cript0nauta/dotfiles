7465aaef8b729d76832d933108c96fb49756ac19
Matías Lang <yo@matiaslang.me>
2020-03-21 15:51:14 -0300

    Use slstatus in dwm_autostart.sh

--- dwm_autostart.sh
+++ dwm_autostart.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
 
-echo $$ >/tmp/dwm_autostart.pid
-
 xsetroot -solid '#002b36'
 xautolock -time 5 -locker i3lock-pixeled -detectsleep &
-
-while true
-do
-    xsetroot -name "$(acpi | tr '\n' ';') / $(date)"
-    sleep 1
-done
-
-rm /tmp/dwm_autostart.pid
+slstatus &
