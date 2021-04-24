4d068061faee8c4a00b8a796e7584f73a3ba3768
Matías Lang <yo@matiaslang.me>
2021-04-24 19:21:57 -0300

    Start firefox, thunderbird and telegram on startup
    And use pgrep to prevent opening a process many times.

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 xsetroot -solid '#002b36'
-xautolock -time 5 -locker lock -detectsleep &
+pgrep xautolock -time 5 -locker lock -detectsleep &
 slstatus &
 xbindkeys
+pgrep firefox || firefox &
+pgrep thunderbird || thunderbird &
+pgrep telegram || telegram-desktop &
